29a2c9c319d0130955feac5924b643292aadcc87
babruix <romanalexey@gmail.com>
2017-03-18 04:47:17 +0200

    Small modifications of main component.

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,8 +1,7 @@
-import { Component, trigger, transition, style, animate} from '@angular/core';
-
-declare var Waypoint: any;
-declare var jQuery: any;
-var $:any = jQuery;
+import { Component, trigger, transition, style, animate} from '@angular/core'
+declare var Waypoint: any
+declare var jQuery: any
+var $:any = jQuery
 
 
 @Component({
@@ -27,54 +26,54 @@ var $:any = jQuery;
 })
 export class AppComponent {
 
-  title = 'Open Sans';
-  loadCompleted = false;
+  loadCompleted = false
+
+  contentWayPoints() {
+
+    $('.animate-box').each(function () {
+
+      const currentEl = $(this)
+
+      new Waypoint({
+        element: currentEl[0],
+        handler: function (direction) {
+
+          if (direction === 'down' && !$(this.element).hasClass('animated')) {
+
+            $(this.element).addClass('item-animate')
+            setTimeout(function () {
+
+              $('body .animate-box.item-animate').each(function (k) {
+                const el = $(this)
+
+                setTimeout(function () {
+
+                  const effect = el.data('animate-effect')
+                  if (effect === 'fadeIn') {
+                    el.addClass('fadeIn animated')
+                  } else if (effect === 'fadeInLeft') {
+                    el.addClass('fadeInLeft animated')
+                  } else if (effect === 'fadeInRight') {
+                    el.addClass('fadeInRight animated')
+                  } else {
+                    el.addClass('fadeInUp animated')
+                  }
+
+                  el.removeClass('item-animate')
+                }, k * 50, 'easeInOutExpo')
+              });
+
+            }, 100)
+          }
+        },
+        offset: '85%'
+      })
+    });
+  }
 
   ngOnInit() {
-    this.loadCompleted = true;
+    this.loadCompleted = true
 
-    var contentWayPoints = function() {
-
-      $('.animate-box').each(function() {
-
-        var waypoint = new Waypoint({
-          element: $(this),
-          handler: function(direction) {
-
-            if( direction === 'down' && !$(this.element).hasClass('animated') ) {
-
-              $(this.element).addClass('item-animate');
-
-              setTimeout(function() {
-
-                $('body .animate-box.item-animate').each(function(k) {
-                  var el = $(this);
-                  setTimeout( function () {
-                    var effect = el.data('animate-effect');
-                    if ( effect === 'fadeIn') {
-                      el.addClass('fadeIn animated');
-                    } else if ( effect === 'fadeInLeft') {
-                      el.addClass('fadeInLeft animated');
-                    } else if ( effect === 'fadeInRight') {
-                      el.addClass('fadeInRight animated');
-                    } else {
-                      el.addClass('fadeInUp animated');
-                    }
-
-                    el.removeClass('item-animate');
-                  },  k * 50, 'easeInOutExpo' );
-                });
-
-              }, 100);
-            }
-          },
-          offset: '85%'
-        })
-      });
-
-
-    };
-
-    setTimeout(()=> contentWayPoints(), 0)
+    setTimeout(() => this.contentWayPoints(), 100)
   }
 }
