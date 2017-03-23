3e45bf2132a2c376f15d0f85af9f9718e5c716b4
babruix <romanalexey@gmail.com>
2017-03-23 02:19:49 +0200

    Services items refactored.

--- src/app/ng-services/ng-services.component.ts
+++ src/app/ng-services/ng-services.component.ts
@@ -7,6 +7,16 @@ import { Component, OnInit } from '@angular/core';
 })
 export class NgServicesComponent implements OnInit {
 
+
+  servicesItems = [
+    {'heading': 'Retina Ready', 'icon': 'icon-eye', 'text': 'Far far away, behind the word mountains, far from the countries Vokalia and Consonantia, there live the blind texts.', 'link': '#'},
+    {'heading': 'Fully Responsive', 'icon': 'icon-command', 'text': 'Far far away, behind the word mountains, far from the countries Vokalia and Consonantia, there live the blind texts.', 'link': '#'},
+    {'heading': 'Web Starter', 'icon': 'icon-power', 'text': 'Far far away, behind the word mountains, far from the countries Vokalia and Consonantia, there live the blind texts.', 'link': '#'},
+    {'heading': 'Retina Ready', 'icon': 'icon-eye', 'text': 'Far far away, behind the word mountains, far from the countries Vokalia and Consonantia, there live the blind texts.', 'link': '#'},
+    {'heading': 'Fully Responsive', 'icon': 'icon-command', 'text': 'Far far away, behind the word mountains, far from the countries Vokalia and Consonantia, there live the blind texts.', 'link': '#'},
+    {'heading': 'Web Starter', 'icon': 'icon-power', 'text': 'Far far away, behind the word mountains, far from the countries Vokalia and Consonantia, there live the blind texts.', 'link': '#'},
+  ]
+
   constructor() { }
 
   ngOnInit() {
