beafdaac7ba5a51115886f72228ae09a0d7bf089
babruix <romanalexey@gmail.com>
2017-03-24 18:10:21 +0200

    Animations added for social icons and logo.

--- src/app/ng-social/ng-social.component.ts
+++ src/app/ng-social/ng-social.component.ts
@@ -1,15 +1,28 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, OnInit, AfterViewInit} from '@angular/core';
 
 @Component({
   selector: 'app-ng-social',
   templateUrl: 'ng-social.component.html',
   styleUrls: ['ng-social.component.scss', '/bootstrap-social.css']
 })
-export class NgsocialComponent implements OnInit {
-
+export class NgsocialComponent implements AfterViewInit {
   constructor() { }
 
-  ngOnInit() {
+  ngAfterViewInit(): void {
+
+    const $socialIcons = $('app-ng-social a');
+    $socialIcons.hide()
+    $socialIcons.each((i, el) => {
+      setTimeout(() => {
+        $(el).addClass('bounceIn animated')
+        }, 50 * i)
+    })
+    $socialIcons.show()
+
+    $('#l-logo').hide()
+    setTimeout(() => {
+      $('#l-logo').addClass('lightSpeedIn animated').show()
+    }, 500)
   }
 
 }
