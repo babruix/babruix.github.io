a4e0bf2f2e464cff5eefe46648b45c66d32c4e85
babruix <romanalexey@gmail.com>
2017-04-05 20:54:05 +0300

    Added testimonial photos.

--- src/app/ng-projects/ng-projects.component.ts
+++ src/app/ng-projects/ng-projects.component.ts
@@ -8,9 +8,9 @@ import {Component, OnInit, AfterViewInit} from '@angular/core';
 export class NgProjectsComponent   {
 
   projectsItems = [
-    {'imageUrl': 'assets/images/broadcastmedia.png', 'heading': 'Ericsson Broadcast and Media Services', 'category': 'Full Stack Developer'},
-    {'imageUrl': 'assets/images/awave.png', 'heading': 'Awave AB', 'category': 'Technical Lead'},
-    {'imageUrl': 'assets/images/ericsson.jpg', 'heading': 'Ericsson AB', 'category': 'Senior Backend Developer'},
+    {'imageUrl': '/assets/images/broadcastmedia.png', 'heading': 'Ericsson Broadcast and Media Services', 'category': 'Full Stack Developer'},
+    {'imageUrl': '/assets/images/awave.png', 'heading': 'Awave AB', 'category': 'Technical Lead'},
+    {'imageUrl': '/assets/images/ericsson.jpg', 'heading': 'Ericsson AB', 'category': 'Senior Backend Developer'},
   ]
 
   constructor() { }
--- src/app/ng-testimonial/ng-testimonial.component.ts
+++ src/app/ng-testimonial/ng-testimonial.component.ts
@@ -10,22 +10,22 @@ export class NgTestimonialComponent implements OnInit {
   feedbackItems = [
     [{
       'text': 'Great job. Attention to details. Good communication. Superb skill.',
-      'imageUrl': 'assets/images/person1.jpg',
+      'imageUrl': '/assets/images/mike_adam.jpg',
       'name': 'Mike Adam'
     },
     {
       'text': 'Successfully accomplished all tasks which was assigned to him under the very strict project schedule. ',
-      'imageUrl': 'assets/images/person2.jpg',
+      'imageUrl': '/assets/images/konstantin_marchenko.jpg',
       'name': 'Konstantin Marchenko'
     }],
     [{
       'text': 'I would highly recommend him to others and am eager to work with him in the future.',
-      'imageUrl': 'assets/images/person3.jpg',
+      'imageUrl': '/assets/images/eric_miller.jpg',
       'name': 'Eric Miller'
     },
     {
-      'text': 'e can work on different tasks involving different web technologies and always reach the goals assigned!',
-      'imageUrl': 'assets/images/person1.jpg',
+      'text': 'He can work on different tasks involving different web technologies and always reach the goals assigned!',
+      'imageUrl': '/assets/images/dmitri_osler.jpg',
       'name': 'Dmitri Osler'
     }]
   ]
